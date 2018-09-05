ab7a44bcf40a90a00dd9cadadbeba2ee985748b5
Charles Doutriaux <doutriaux1@llnl.gov>
2018-09-05 13:11:58 -0700

    followed instructions at: https://github.com/jupyter-widgets/widget-cookiecutter/blob/master/%7B%7Bcookiecutter.github_project_name%7D%7D/js/lib/example.js#L24

--- js/src/graphicsmethod.js
+++ js/src/graphicsmethod.js
@@ -1,6 +1,9 @@
+// got syntax at: https://github.com/jupyter-widgets/widget-cookiecutter/blob/master/%7B%7Bcookiecutter.github_project_name%7D%7D/js/lib/example.js#L24
+
+
 var widgets = require('@jupyter-widgets/base');
 
-var _ = require('underscore');
+var _ = require('lodash');
 var React = require('react');
 var ReactDOM = require('react-dom');
 var VCSWidgets = require('vcs-widgets');
@@ -13,69 +16,50 @@ var VCSWidgets = require('vcs-widgets');
 // When serialiazing entire widget state for embedding, only values different from the
 // defaults will be specified.
 
-define(["@jupyter-widgets/base"], function(widgets) { 
-
-var GMModel = widgets.DOMWidgetModel.extend({
-
-    var GmModel = widgets.DOMWidgetModel.extend({
-        defaults: _.extend(widgets.DOMWidgetModel.prototype.defaults, {
-            _model_name: 'GMModel',
-            _view_name: 'GMView',
-            _model_module: 'cdat-notebook-widgets',
-            _view_module: 'cdat-notebook-widgets',
-            _model_module_version : '~1.0.0',
-            _view_module_version : '~1.0.0',
-            value: {"g_name": "Gfb"},
-            colormaps: ["default"]
-        }),
-    });
-
-    return {
-        GmModel: GmModel
-    };
+var GmModel = widgets.DOMWidgetModel.extend({
+    defaults: _.extend(widgets.DOMWidgetModel.prototype.defaults(), {
+        _model_name: 'GMModel',
+        _view_name: 'GMView',
+        _model_module: 'cdat-notebook-widgets',
+        _view_module: 'cdat-notebook-widgets',
+        _model_module_version: '~1.0.0',
+        _view_module_version: '~1.0.0',
+        value: { "g_name": "Gfb" },
+        colormaps: ["default"]
+    }),
 });
 
-
-
 // Custom View. Renders the widget model.
-require.undef('gm');
-
-define(["@jupyter-widgets/base"], function(widgets) {
 
     // Define the HelloView
-    var GmView = widgets.DOMWidgetView.extend({
-        render: function() {
-            this.value_changed();
-            this.model.on('change:value', this.value_changed, this);
+var GmView = widgets.DOMWidgetView.extend({
+    render: function () {
+        this.value_changed();
+        this.model.on('change:value', this.value_changed, this);
         },
-        value_changed: function() {
-            var gm = this.model.get("value");
-            var self = this;
-            function saveChanges() {
-                self.touch();
-            }
-            function updateActiveGM(props) {
-                self.model.set("value", props);
-            }
-            var gms = {}
-            gms[gm.g_name] = {}
-            gms[gm.g_name][gm.name] = gm;
-            var props = {
-                graphicsMethod: gm.name,
-                graphicsMethodParent: gm.g_name,
-                gmProps: gm,
-                graphicsMethods: gms,
-                updateGraphicsMethods: saveChanges,
-                updateActiveGM: updateActiveGM,
-                colormaps: this.model.get("colormaps")
-            }
-            var component = React.createElement(VCSWidgets.GMEdit, props);
-            ReactDOM.render(component, this.el);
+    value_changed: function () {
+        var gm = this.model.get("value");
+        var self = this;
+        function saveChanges() {
+            self.touch();
         }
-    });
-
-    return {
-        GmView: GmView
+        function updateActiveGM(props) {
+            self.model.set("value", props);
+        }
+        var gms = {}
+        gms[gm.g_name] = {}
+        gms[gm.g_name][gm.name] = gm;
+        var props = {
+            graphicsMethod: gm.name,
+            graphicsMethodParent: gm.g_name,
+            gmProps: gm,
+            graphicsMethods: gms,
+            updateGraphicsMethods: saveChanges,
+            updateActiveGM: updateActiveGM,
+            colormaps: this.model.get("colormaps")
+        }
+        var component = React.createElement(VCSWidgets.GMEdit, props);
+        ReactDOM.render(component, this.el);
     }
 });
 
